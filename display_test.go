6e0797e6b9408a554746f3960e9ee4107af80c5f
Stefan Mühlebach <stefan.muehlebach@gmx.net>
2024-01-07 01:13:52 +0100

    'LiteIDE'

--- display_test.go
+++ display_test.go
@@ -40,7 +40,7 @@ func init() {
     disp = OpenDisplay(Rotate000)
     fWidth, fHeight = float64(Width), float64(Height)
 
-    pixBuf = NewBuffer(Width, Height)
+    pixBuf = NewBuffer(image.Rect(0, 0, Width, Height))
 
     fh, err := os.Open(imageFile)
     if err != nil {
