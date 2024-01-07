bfa410fd4f6829da9837743e8aa9d9803e6b21c0
Stefan Mühlebach <stefan.muehlebach@gmx.net>
2024-01-07 12:04:44 +0100

    'LiteIDE'

--- display_test.go
+++ display_test.go
@@ -97,7 +97,7 @@ func TestDrawSyncCust(t *testing.T) {
     gc.Clear()
     disp.DrawSync(gc.Image())
     disp.DrawSync(testImage.SubImage(RectCust))
-    disp.DrawSync(testImage.SubImage(RectCust.Add(image.Pt(120,60))))
+    // disp.DrawSync(testImage.SubImage(RectCust.Add(image.Pt(120,60))))
 //    disp.DrawSync(testImage.SubImage(RectCust.Add(image.Pt(200,150))))
 }
 
